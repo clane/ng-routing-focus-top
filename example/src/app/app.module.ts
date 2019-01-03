be69775181e4178f747649e9f03e5cd891c6768c
Chris Lane <chris.lane359@gmail.com>
2019-01-03 06:46:41 -0800

    routing works

--- example/src/app/app.module.ts
+++ example/src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { Component1Component } from './component1/component1.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Component1Component
   ],
   imports: [
     BrowserModule
+		RouterModule.forRoot([
+
+    {
+      //A default path is required to prevent routing errors
+      path:'',
+      component: Component1Component,
+
+    },
+    {
+      path: 'component1',
+      component: Component1Component,
+    },
+  ])
   ],
   providers: [],
   bootstrap: [AppComponent]

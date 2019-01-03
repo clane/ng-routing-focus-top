a6de56984895f7612e4ad21b53d52751f0901749
Chris Lane <chris.lane359@gmail.com>
2019-01-03 07:13:57 -0800

    added the other components, matched the markup from the React example, all routes work

--- example/src/app/app.module.ts
+++ example/src/app/app.module.ts
@@ -4,12 +4,18 @@ import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { Component1Component } from './component1/component1.component';
+import { Component2Component } from './component2/component2.component';
+import { Component3Component } from './component3/component3.component';
+import { Component4Component } from './component4/component4.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    Component1Component
+    Component1Component,
+    Component2Component,
+    Component3Component,
+    Component4Component
   ],
   imports: [
     BrowserModule
@@ -24,6 +30,18 @@ import { Component1Component } from './component1/component1.component';
     {
       path: 'component1',
       component: Component1Component,
+    },
+		{
+      path: 'component2',
+      component: Component2Component,
+    },
+		{
+      path: 'component3',
+      component: Component3Component,
+    },
+		{
+      path: 'component4',
+      component: Component4Component,
     },
   ])
   ],

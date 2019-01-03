4733591965e0dbc8f4a9fa6c77ba9746f8b4b742
Chris Lane <chris.lane359@gmail.com>
2019-01-03 15:00:47 -0800

    Added other component files and began setting up to manage aria-current on the router links

--- example/src/app/app.component.ts
+++ example/src/app/app.component.ts
@@ -10,7 +10,12 @@ export class AppComponent {
 
 constructor(private routingSignalService: RoutingSignalService) { }
 
-@ViewChild('top') elementToFocusAfterRouting;
+	@ViewChild('top') elementToFocusAfterRouting;
+
+  ariaCurrent1:string = "test";
+  ariaCurrent2:string = null;
+  ariaCurrent3:string = null;
+  ariaCurrent4:string = null;
 
   ngOnInit() {
      this.routingSignalService.onRoute.subscribe( (e) => {
@@ -25,5 +30,29 @@ constructor(private routingSignalService: RoutingSignalService) { }
 					 setTimeout(()=>{this.elementToFocusAfterRouting.nativeElement.focus();}, 500);
         }
    }
+
+ setCurrentRouterLink(componentName) {
+    //Set aria-current
+    //Reset aria-current on all router links first
+      this.ariaCurrent1 = null;
+      this.ariaCurrent2 = null;
+      this.ariaCurrent3 = null;
+      this.ariaCurrent4 = null;
+
+
+    if (componentName === "component 1") {
+        this.ariaCurrent1 =  "page"; 
+    } 
+    if (componentName === "component 2") {
+        this.ariaCurrent2 =  "page"; 
+    }
+    if (componentName === "component 3") {
+        this.ariaCurrent3 =  "page"; 
+    }
+    if (componentName === "component 4") {
+        this.ariaCurrent4 =  "page"; 
+    }
+ }
+
 
 }
